f67495517ca8e66b108f4effb69a428cde946b57
Pablo Leano <pleasedontbelong@gmail.com>
2021-11-04 08:18:26 +0100

    Rollback to previous version, we should not be able to select the same track twice

--- groover/smart.py
+++ groover/smart.py
@@ -3,26 +3,29 @@ from groover.bruteforce import MAX_NB_TRACKS
 
 def tracks_match(tracks, concert_premiere_length):
     """
+    Proxy for _find_three_tracks, just to be sure we sort the list
+    before calling it
+    """
+    # first we need to sort the track lengths
+    tracks.sort()
+    return _find_three_tracks(tracks, total_length=concert_premiere_length)
+
+
+def _find_three_tracks(tracks, total_length):
+    """
     Returns True if the sum of three tracks on the `tracks` list are
     equal to `concert_premiere_length`
     """
-    # generate an array of False with length equal to concert_premiere_length
-    results = [False for i in range(concert_premiere_length + 1)]
-    # for each index we'll find if there's a sum of two tracks that are equal
-    # to the index
-    for i in range(len(results)):
-        results[i] = _find_two_tracks(tracks, i)
+    if len(tracks) < 3:
+        return False
 
-    # loop over every track and we'll use the results array to check if there's
-    # a pair of tracks that have a sum equal to the difference of the `concert_premiere_length`
-    # and the `track`
-    for track in tracks:
-        try:
-            if results[concert_premiere_length - track]:
-                return True
-        except IndexError:
-            continue
-    return False
+    last = tracks.pop()
+    # find two tracks with the length equal to (total_length - last)
+    if _find_two_tracks(tracks, total_length - last):
+        return True
+    else:
+        # the last track is useless, we repeat the operation without it
+        return _find_three_tracks(tracks, total_length)
 
 
 def _find_two_tracks(tracks, total_length):
